f68840a81937517c9e9ce09180a45bd69ac3f9f9
Syafiqah Sahrom <syafiqah@Syafiqahs-MacBook-Pro.local>
2026-02-23 20:58:29 +0800

    Fix CORS error for semakbijak.com

--- pages/api/semak/bulk.js
+++ pages/api/semak/bulk.js
@@ -50,6 +50,15 @@ function parseForm(req) {
 }
 
 export default async function handler(req, res) {
+res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', 'https://semakbijak.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+
+  // Handle the browser "preflight" check
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
 
   let fields, files;
